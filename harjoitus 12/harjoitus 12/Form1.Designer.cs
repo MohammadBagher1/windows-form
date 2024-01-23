25e117c025fe1f2b21f596a97e9dd49fc9a1b626
Mohammad Bagheri <mohammad.bagheri@edu.keuda.fi>
2024-01-23 10:52:48 +0200

    12

--- harjoitus 12/harjoitus 12/Form1.Designer.cs
+++ harjoitus 12/harjoitus 12/Form1.Designer.cs
@@ -30,10 +30,11 @@
         {
             this.KysymysLB = new System.Windows.Forms.Label();
             this.OptionGB = new System.Windows.Forms.GroupBox();
-            this.VastausARB = new System.Windows.Forms.RadioButton();
-            this.VastausBRB = new System.Windows.Forms.RadioButton();
-            this.VastausCRB = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.VastausDRB = new System.Windows.Forms.RadioButton();
+            this.VastausCRB = new System.Windows.Forms.RadioButton();
+            this.VastausBRB = new System.Windows.Forms.RadioButton();
+            this.VastausARB = new System.Windows.Forms.RadioButton();
             this.VastausLB = new System.Windows.Forms.Label();
             this.OptionGB.SuspendLayout();
             this.SuspendLayout();
@@ -54,6 +55,7 @@
             this.OptionGB.Controls.Add(this.VastausCRB);
             this.OptionGB.Controls.Add(this.VastausBRB);
             this.OptionGB.Controls.Add(this.VastausARB);
+            this.OptionGB.Controls.Add(this.radioButton1);
             this.OptionGB.Location = new System.Drawing.Point(426, 39);
             this.OptionGB.Name = "OptionGB";
             this.OptionGB.Size = new System.Drawing.Size(110, 206);
@@ -61,32 +63,34 @@
             this.OptionGB.TabStop = false;
             this.OptionGB.Text = "Vasta: ";
             // 
-            // VastausARB
+            // radioButton1
             // 
-            this.VastausARB.AutoSize = true;
-            this.VastausARB.Location = new System.Drawing.Point(19, 56);
-            this.VastausARB.Name = "VastausARB";
-            this.VastausARB.Size = new System.Drawing.Size(59, 29);
-            this.VastausARB.TabIndex = 0;
-            this.VastausARB.TabStop = true;
-            this.VastausARB.Text = "A. ";
-            this.VastausARB.UseVisualStyleBackColor = true;
+            this.radioButton1.AutoCheck = false;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 54);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 29);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "E";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
             // 
-            // VastausBRB
+            // VastausDRB
             // 
-            this.VastausBRB.AutoSize = true;
-            this.VastausBRB.Location = new System.Drawing.Point(19, 91);
-            this.VastausBRB.Name = "VastausBRB";
-            this.VastausBRB.Size = new System.Drawing.Size(59, 29);
-            this.VastausBRB.TabIndex = 1;
-            this.VastausBRB.TabStop = true;
-            this.VastausBRB.Text = "B. ";
-            this.VastausBRB.UseVisualStyleBackColor = true;
+            this.VastausDRB.AutoSize = true;
+            this.VastausDRB.Location = new System.Drawing.Point(6, 159);
+            this.VastausDRB.Name = "VastausDRB";
+            this.VastausDRB.Size = new System.Drawing.Size(60, 29);
+            this.VastausDRB.TabIndex = 3;
+            this.VastausDRB.TabStop = true;
+            this.VastausDRB.Text = "D. ";
+            this.VastausDRB.UseVisualStyleBackColor = true;
             // 
             // VastausCRB
             // 
             this.VastausCRB.AutoSize = true;
-            this.VastausCRB.Location = new System.Drawing.Point(19, 126);
+            this.VastausCRB.Location = new System.Drawing.Point(6, 124);
             this.VastausCRB.Name = "VastausCRB";
             this.VastausCRB.Size = new System.Drawing.Size(60, 29);
             this.VastausCRB.TabIndex = 2;
@@ -94,16 +98,27 @@
             this.VastausCRB.Text = "C. ";
             this.VastausCRB.UseVisualStyleBackColor = true;
             // 
-            // VastausDRB
+            // VastausBRB
             // 
-            this.VastausDRB.AutoSize = true;
-            this.VastausDRB.Location = new System.Drawing.Point(19, 161);
-            this.VastausDRB.Name = "VastausDRB";
-            this.VastausDRB.Size = new System.Drawing.Size(60, 29);
-            this.VastausDRB.TabIndex = 3;
-            this.VastausDRB.TabStop = true;
-            this.VastausDRB.Text = "D. ";
-            this.VastausDRB.UseVisualStyleBackColor = true;
+            this.VastausBRB.AutoSize = true;
+            this.VastausBRB.Location = new System.Drawing.Point(6, 89);
+            this.VastausBRB.Name = "VastausBRB";
+            this.VastausBRB.Size = new System.Drawing.Size(59, 29);
+            this.VastausBRB.TabIndex = 1;
+            this.VastausBRB.TabStop = true;
+            this.VastausBRB.Text = "B. ";
+            this.VastausBRB.UseVisualStyleBackColor = true;
+            // 
+            // VastausARB
+            // 
+            this.VastausARB.AutoSize = true;
+            this.VastausARB.Location = new System.Drawing.Point(6, 54);
+            this.VastausARB.Name = "VastausARB";
+            this.VastausARB.Size = new System.Drawing.Size(59, 29);
+            this.VastausARB.TabIndex = 0;
+            this.VastausARB.TabStop = true;
+            this.VastausARB.Text = "A. ";
+            this.VastausARB.UseVisualStyleBackColor = true;
             // 
             // VastausLB
             // 
@@ -125,7 +140,7 @@
             this.Controls.Add(this.OptionGB);
             this.Controls.Add(this.KysymysLB);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.OptionGB.ResumeLayout(false);
@@ -144,6 +159,7 @@
         private System.Windows.Forms.RadioButton VastausBRB;
         private System.Windows.Forms.RadioButton VastausARB;
         private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
